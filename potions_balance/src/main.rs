7a6c2c52a7a7783a76ebbc2ceff336b50229b426
Warlock <internalmike@gmail.com>
2020-03-24 02:41:12 +0300

    potions_balance

--- potions_balance/src/main.rs
+++ potions_balance/src/main.rs
@@ -1,3 +1,10 @@
+#![windows_subsystem = "windows"]
+
+use std::ptr::{self};
+use winapi::um::winuser::MessageBoxW;
+
 fn main() {
-    println!("Hello, world!");
+    let caption = "caption\0".encode_utf16().collect::<Vec<_>>();
+    let message = "hello, world\0".encode_utf16().collect::<Vec<_>>();
+    unsafe { MessageBoxW(ptr::null_mut(), message.as_ptr(), caption.as_ptr(), 0); }
 }
